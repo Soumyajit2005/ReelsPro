4650bd0313eac8cf8592fbfce8d665004f452e32
Soumyajit <soumyajitparia2005@gmail.com>
2025-08-17 20:47:03 +0530

    Implement user registration API with email and password validation

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDataBase } from "@/lib/db";
+import User from "@/models/User";
+
+export async function POST(request: NextRequest) {
+    try {
+        const { email, password } = await request.json()
+
+        if (!email || !password) {
+            return NextResponse.json(
+                { error: "Email and password are required" },
+                { status: 400 }
+            )
+        }
+
+        await connectToDataBase()
+
+        const existingUser = await User.findOne({ email })
+
+        if (existingUser) {
+            return NextResponse.json(
+                { error: "Email is already registered" },
+                { status: 400 }
+            )
+        }
+
+        await User.create({
+            email,
+            password
+        })
+
+        return NextResponse.json(
+            { message: "User registered successfuly" },
+            { status: 201 }
+        )
+
+
+    } catch (error) {
+        return NextResponse.json(
+            { error: `error: ${error}` },
+            { status: 500 }
+        )
+    }
+}
